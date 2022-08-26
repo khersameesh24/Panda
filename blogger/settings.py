2281a788aa3da97671bba2528df7c6984ffa5b21
khersameesh24 <khersameesh24@gmail.com>
2022-08-26 22:55:36 +0530

    hide secret

--- blogger/settings.py
+++ blogger/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 import json
 from pathlib import Path
+from dotenv import load_dotenv
 
 try:
     with open("config/.config.json", "r") as f:
@@ -27,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config["debug"]
